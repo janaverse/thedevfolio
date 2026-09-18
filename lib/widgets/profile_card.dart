54d802e7393d1993e9c525ceac879977016ef270
Jana <ijana.alharbi@gmail.com>
2026-09-19 02:53:25 +0300

    Fix CV link

--- lib/widgets/profile_card.dart
+++ lib/widgets/profile_card.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/link.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../theme/app_theme.dart';
-import 'dart:html' as html;
 
 class ProfileCard extends StatelessWidget {
   const ProfileCard({super.key});
@@ -36,13 +36,6 @@ class ProfileCard extends StatelessWidget {
     );
 
     await launchUrl(emailUri);
-  }
-
-  void _openCV() {
-    html.window.open(
-      'https://thedevfolio.netlify.app/certificates/Jana_Almeziney_CV.pdf',
-      '_blank',
-    );
   }
 
   @override
@@ -155,14 +148,22 @@ class ProfileCard extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 30),
-          SizedBox(
-            width: double.infinity,
-            height: 48,
-            child: OutlinedButton.icon(
-              onPressed: _openCV,
-              icon: const Icon(Icons.download),
-              label: const Text('Download CV'),
+          Link(
+            uri: Uri.parse(
+              'https://thedevfolio.netlify.app/certificates/Jana_Almeziney_CV.pdf',
             ),
+            target: LinkTarget.blank,
+            builder: (context, openLink) {
+              return SizedBox(
+                width: double.infinity,
+                height: 48,
+                child: OutlinedButton.icon(
+                  onPressed: openLink,
+                  icon: const Icon(Icons.download),
+                  label: const Text('Download CV'),
+                ),
+              );
+            },
           ),
         ],
       ),
